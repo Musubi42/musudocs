8330cff2d3b666e04fae1359202221395821acf4
Musubi42 <raphaelthi59@gmail.com>
2024-11-13 17:23:31 +0100

    lets go

--- docs/posts/[id].paths.js
+++ docs/posts/[id].paths.js
@@ -1,34 +1,34 @@
-// posts/[id].paths.js
-import { loadEnv } from 'vitepress'
+// // posts/[id].paths.js
+// import { loadEnv } from 'vitepress'
 
-const env = loadEnv('development', process.cwd())
+// const env = loadEnv('development', process.cwd())
 
-export default {
-  async paths() {
-    const response = await fetch('https://backoffice.roulerpouraider.fr/api/vite-presses', {
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${env.VITE_STRAPI_API_TOKEN}`
-      }
-    })
-    const { data } = await response.json()
+// export default {
+//   async paths() {
+//     const response = await fetch('https://backoffice.roulerpouraider.fr/api/vite-presses', {
+//       headers: {
+//         "Content-Type": "application/json",
+//         'Authorization': `Bearer ${env.VITE_STRAPI_API_TOKEN}`
+//       }
+//     })
+//     const { data } = await response.json()
 
-    console.log(data);
+//     console.log(data);
 
-    return data.map((pkg) => {
-      return {
-        params: {
-          id: pkg.id,
-          title: pkg.attributes.Title
-        }
-      }
-    })
-    // const result = data.map(entry => ({
-    //   params: { id: entry.id },
-    //   title: entry.attributes.Title,
-    // }))
+//     return data.map((pkg) => {
+//       return {
+//         params: {
+//           id: pkg.id,
+//           title: pkg.attributes.Title
+//         }
+//       }
+//     })
+//     // const result = data.map(entry => ({
+//     //   params: { id: entry.id },
+//     //   title: entry.attributes.Title,
+//     // }))
 
-    // console.log("je suis un ", result);
+//     // console.log("je suis un ", result);
 
-  }
-}
+//   }
+// }
